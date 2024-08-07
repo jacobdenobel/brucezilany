67de43c941cd019a2205f04c418cc856e9c47036
jacobdenobel <jacobdenobel@gmail.com>
2024-08-07 16:14:57 +0200

    attempt

--- bruce/__main__.py
+++ bruce/__main__.py
@@ -11,7 +11,7 @@ FIGURES = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data/figures
 
 if __name__ == "__main__":
     os.makedirs(FIGURES, exist_ok=True)
-    
+
     parser = argparse.ArgumentParser("Create a neurogram for a given WAV file at path")
     parser.add_argument("path", type=str)
     parser.add_argument("--n_cf", default=40, type=int)
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("--bin_width", default=5e-4, type=float)
     parser.add_argument("--plot", action="store_true")
     args = parser.parse_args()
-    
+
     if not os.path.isfile(args.path):
         raise FileNotFoundError(args.path + " not found")
 
@@ -31,22 +31,19 @@ if __name__ == "__main__":
     ng = bruce.Neurogram(args.n_cf)
     ng.bin_width = args.bin_width
     ng.create(stim, args.n_rep)
-    
+
     binned_output = ng.get_unfiltered_output()
-    y = ng.get_cfs()  
+    y = ng.get_cfs()
     t = np.arange(binned_output.shape[1]) * args.bin_width
-    
+
     if args.plot:
         plt.figure(figsize=(9, 4))
-        plt.pcolor(t, y / 1e3, binned_output, cmap="viridis", vmin=0, vmax=binned_output.max())
+        plt.pcolor(
+            t, y / 1e3, binned_output, cmap="viridis", vmin=0, vmax=binned_output.max()
+        )
         plt.yscale("log")
         plt.ylabel("frequency")
         plt.xlabel("time [s]")
         plt.colorbar()
-        plt.savefig(f".{FIGURES}/{os.path.basename(args.path)}_neurogram.png")
+        plt.savefig(f"{FIGURES}/{os.path.basename(args.path)}_neurogram.png")
         # plt.show()
-        
-                   
-
-
-    
